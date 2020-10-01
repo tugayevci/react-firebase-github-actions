900b8ce4384bec50d77e367403d23968bb70c39a
Tugay Cem Evci <tevci@nuevo.com.tr>
2020-10-01 14:17:47 +0300

    full chat app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
 import { useAuthState } from "react-firebase-hooks/auth";
-// import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import firebaseconfig from "./firebaseconfig";
 
 firebase.initializeApp(firebaseconfig);
 
 const auth = firebase.auth();
-// const firestore = firebase.firestore();
+const firestore = firebase.firestore();
 
 function App() {
   const [user] = useAuthState(auth);
@@ -35,8 +35,85 @@ export function SignIn() {
   return <button onClick={signInWithGoogle}>Sign in with Google</button>;
 }
 
+function SignOut() {
+  return auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
 function ChatRoom() {
-  return <></>;
+  const dummy = useRef(null);
+
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  useEffect(() => {
+    if (dummy) {
+      //@ts-ignore
+      dummy.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
+  return (
+    <>
+      <main>
+        {messages?.map((msg) => (
+          <ChatMessage
+            //@ts-ignore
+            key={msg.id}
+            message={msg}></ChatMessage>
+        ))}
+        <div ref={dummy}></div>
+      </main>
+      <ChatMessageForm scrollDownRef={dummy} />
+    </>
+  );
+}
+
+function ChatMessageForm(props: any) {
+  const [message, setMessage] = useState("");
+
+  const sendMessage = async (e: any) => {
+    e.preventDefault();
+    const messagesRef = firestore.collection("messages");
+
+    //@ts-ignore
+    const { uid, photoURL } = auth.currentUser;
+
+    console.log("uid", uid);
+    console.log("photoURL", photoURL);
+
+    await messagesRef.add({
+      text: message,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setMessage("");
+    props.scrollDownRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <form onSubmit={sendMessage}>
+      <input value={message} onChange={(e: any) => setMessage(e.target.value)}></input>
+      <button disabled={message.length < 1} type="submit">
+        Send
+      </button>
+    </form>
+  );
+}
+
+function ChatMessage(props: any) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser?.uid ? "sent" : "received";
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL}></img>
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default App;
