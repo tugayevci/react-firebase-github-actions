43069dd4227ac6178f229425dcfc369b8fd48b61
Tugay Cem Evci <tevci@nuevo.com.tr>
2020-10-01 13:17:57 +0300

    minor update

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBVtN3WUgrlyk1u9TJbmoOcsm6qi-0lCTg",
+  authDomain: "money-talks-b9ddd.firebaseapp.com",
+  databaseURL: "https://money-talks-b9ddd.firebaseio.com",
+  projectId: "money-talks-b9ddd",
+  storageBucket: "money-talks-b9ddd.appspot.com",
+  messagingSenderId: "427382149125",
+  appId: "1:427382149125:web:6295a9efe6e2f570564c6c",
+};
