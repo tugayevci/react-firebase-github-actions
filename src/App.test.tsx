b855e9f206eca3dc15d4872fd8f0fe6005a6ca47
Tugay Cem Evci <tevci@nuevo.com.tr>
2020-10-01 10:22:29 +0300

    sign in added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App, { SignIn } from "./App";
 
-test('renders learn react link', () => {
+test("Money Talks header check", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = getByText(/Money Talks/i);
+  expect(text).toBeInTheDocument();
+});
+
+test("login button check", () => {
+  const { getByText } = render(<SignIn />);
+  const loginButton = getByText(/sign in/i);
+  expect(loginButton).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import firebaseconfig from "./firebaseconfig";
+
+firebase.initializeApp(firebaseconfig);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Money Talks</h4>
       </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
+}
+
+export function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function ChatRoom() {
+  return <></>;
 }
 
 export default App;
